6e08078e28628109f34867b975ae7eb98edbe549
Tony Worm <verdverm@gmail.com>
2018-02-02 15:28:53 -0700

    passing ci

--- src/test-basic.test.js
+++ src/test-basic.test.js
@@ -3,21 +3,21 @@
 const Connector = require('./index');
 
 const Foo = {
-  name: "Foo",
-  do: () => "doing something...",
-}
+  name: 'Foo',
+  do: () => 'doing something...',
+};
 
 const Boo = {
-  name: "Casper",
-  do: () => "being friendly",
-  say: () => "Boooooooo!",
-}
+  name: 'Casper',
+  do: () => 'being friendly',
+  say: () => 'Boooooooo!',
+};
 
 const Moo = {
-  name: "Cow",
-  say: () => "MoooOOOO",
-  do: () => "doing that farm to table thing...",
-}
+  name: 'Cow',
+  say: () => 'MoooOOOO',
+  do: () => 'doing that farm to table thing...',
+};
 
 test('new connector is empty', () => {
   let conn = new Connector.Connector();
@@ -30,7 +30,7 @@ test('can add items to a connector', () => {
   conn.Add(Foo);
   expect(conn.Items().length).toEqual(1);
 
-  conn.Add(Boo,Moo);
+  conn.Add(Boo, Moo);
   expect(conn.Items().length).toEqual(3);
 });
 
@@ -38,7 +38,7 @@ test('can add connector to a connector', () => {
   let connA1 = new Connector.Connector();
   connA1.Add(Foo);
   let connA2 = new Connector.Connector();
-  connA2.Add(Boo,Moo);
+  connA2.Add(Boo, Moo);
   connA1.Add(connA2);
 
   expect(connA1.Items().length).toEqual(3);
@@ -46,51 +46,50 @@ test('can add connector to a connector', () => {
 
 test('can get items from a connector', () => {
   let conn = new Connector.Connector();
-  conn.Add(Foo,Boo,Moo);
+  conn.Add(Foo, Boo, Moo);
 
   let spec1 = {
-    name: "name",
-  }
+    name: 'name',
+  };
   let spec2 = {
-    name: "name",
-    do: () => "do",
-  }
+    name: 'name',
+    do: () => 'do',
+  };
   let spec3 = {
-    name: "name",
-    say: () => "say",
-  }
+    name: 'name',
+    say: () => 'say',
+  };
 
   expect(conn.Get(spec1).length).toEqual(3);
   expect(conn.Get(spec2).length).toEqual(3);
   expect(conn.Get(spec3).length).toEqual(2);
 });
 
-
 test('can Connect() items in a connector', () => {
   let conn = new Connector.Connector();
-  conn.Add(Foo,Boo,Moo);
+  conn.Add(Foo, Boo, Moo);
 
   let spec1 = {
-    name: "name",
-  }
+    name: 'name',
+  };
   let spec2 = {
-    name: "name",
-    say: () => "say",
-  }
+    name: 'name',
+    say: () => 'say',
+  };
 
   let multi1 = {
     things: [],
   };
-  multi1.Connect = (connector) => {
-    multi1.things = connector.Get(spec1)
-  }
+  multi1.Connect = connector => {
+    multi1.things = connector.Get(spec1);
+  };
 
   let multi2 = {
     things: [],
   };
-  multi2.Connect = (connector) => {
-    multi2.things = connector.Get(spec2)
-  }
+  multi2.Connect = connector => {
+    multi2.things = connector.Get(spec2);
+  };
 
   conn.Add(multi1, multi2);
 
@@ -99,5 +98,3 @@ test('can Connect() items in a connector', () => {
   expect(multi1.things.length).toEqual(3);
   expect(multi2.things.length).toEqual(2);
 });
-
-
